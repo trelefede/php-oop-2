8098c150bac3cdbdf4c6f4ce404bd2d056a3139b
TreleaniFederico <treleani.federico@gmail.com>
2022-10-26 17:16:13 +0200

    add users add products

---
+++ models/Users.php
@@ -0,0 +1,15 @@
+<?php
+    class User {
+        public $nome;
+        public $cognome;
+        public $email;
+        public $data_nascita;
+
+        private $password;
+        
+    }
+
+
+
+
+?>
