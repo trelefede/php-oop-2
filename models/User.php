f36551f1362abbc5ea0c2a2df7e853e6967da904
TreleaniFederico <treleani.federico@gmail.com>
2022-10-27 15:32:47 +0200

    add class user function addProduct in cart[]

---
+++ models/User.php
@@ -0,0 +1,19 @@
+<?php
+    class User {
+
+        public $nome;
+        public $cognome;
+        public $email;
+        public $data_nascita;
+
+        public $cart = [];
+
+        public function addProduct($product){
+            $this->cart[] = $product;
+        }
+
+        
+        
+    }
+
+?>
